327fa761ac776b8ece5d248bf2b08b4a4c2802be
EricksonMela <erickmela2910@gmail.com>
2024-01-16 17:37:39 -0500

    Modelo principal y el service

--- AngularFrontend/frontend/src/app/app-routing.module.ts
+++ AngularFrontend/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'principal',component:PrincipalComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularFrontend/frontend/src/app/app.module.ts
+++ AngularFrontend/frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
