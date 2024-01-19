f1f6f100b69d53b9ed1284562a8bddbe9786ccf8
EricksonMela <erickmela2910@gmail.com>
2024-01-19 10:45:22 -0500

    Mejorando la vista principal

--- AngularFrontend/frontend/src/app/app-routing.module.ts
+++ AngularFrontend/frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
 import { BusquedaComponent } from './busqueda/busqueda.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
   {path:'principal',component:PrincipalComponent},
   {path:'busqueda',component:BusquedaComponent},
+  {path:'registro',component:RegistroComponent},
 ];
 
 @NgModule({
--- AngularFrontend/frontend/src/service/service.service.ts
+++ AngularFrontend/frontend/src/service/service.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
-
+import { Usuario } from 'src/model/Usuario.model';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+  private ApiUrl = "http://127.0.0.1:8000/api"; // URL to web api
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
 
-  constructor() { }
+  };
+  constructor(private http: HttpClient) { }
+
+
+  //USUARIO---
+    
+  public getUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.ApiUrl+ 'Usuario/',this.httpOptions)
+  }
+
+public postUsuarios(usu:Usuario):Observable<void>{
+  let body = JSON.stringify(usu);
+  return this.http.post<void>(this.ApiUrl + 'Usuario/',body,this.httpOptions);
+  }
+
+public deleteUsuario(id:string): Observable<void> {
+  return this.http.delete<void>(this.ApiUrl + 'Usuario/' + id + '/',this.httpOptions)
+  }
+
+ public putUsuario(usu:Usuario):Observable<void>{
+  let body = JSON.stringify(usu);
+  return this.http.put<void>(this.ApiUrl + 'Usuario/' + usu.id + '/',body,this.httpOptions);
+  }
 }
